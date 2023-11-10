4c6bf38f7f01a0812ca9e301421959fa69c4caac
JacobSandin <jacob@js.se>
2023-11-10 15:11:51 +0100

    Added SQL to dict

--- classes/DataUtils.py
+++ classes/DataUtils.py
@@ -50,6 +50,53 @@ class DataUtils(AddValues):
         raise Exception(f"Failed to ping MySQL server after {max_retries} retries. Connection may be dead.")
 
 
+    def sql_dict(self, query, args=None, return_data=False, many=False, warn_seconds=2, return_id=False):
+        try:
+            connection = self.get_mysql_connection()
+            cursor = connection.cursor()
+
+            start_time = time.time()
+
+            # Execute the query with arguments
+            if args is not None:
+                if many:
+                    cursor.executemany(query, args)
+                else:
+                    cursor.execute(query, args)
+            else:
+                if many:
+                    cursor.executemany(query)
+                else:
+                    cursor.execute(query)
+
+            # Commit the changes
+            connection.commit()
+
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= warn_seconds:
+                query2 = re.sub(r'\s+', ' ', query)[:100]
+                self.log(f"Warning: Query took {elapsed_time:.2f} seconds to execute: {query2}", level='debug')
+
+            if return_id and return_data:
+                result = cursor.fetchall()
+                columns = [desc[0] for desc in cursor.description]
+                return {result[0][0]: dict(zip(columns[1:], result[0][1:]))}
+            elif return_id:
+                return cursor.lastrowid
+            elif return_data:
+                result = cursor.fetchall()
+                columns = [desc[0] for desc in cursor.description]
+                return {row[0]: dict(zip(columns[1:], row[1:])) for row in result}
+
+            return
+        except Exception as e:
+            query2 = re.sub(r'\s+', ' ', query)[:100]
+            self.log(f"Error running sql: {e} {query2}", level='error')
+            return None
+        finally:
+            if cursor:
+                cursor.close()
+                
     def sql(self, query, args=None, return_data=False, many=False, warn_seconds=2,return_id=False):
         try:
             connection = self.get_mysql_connection()
