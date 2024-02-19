2674214716945efbd33991842cfb6910c55931aa
JacobSandin <jacob@js.se>
2024-02-19 13:48:20 +0100

    Revert pymysqlpool is too much hassle with isntall

--- classes/DataUtils.py
+++ classes/DataUtils.py
@@ -2,43 +2,56 @@ import pymysql
 import re
 import time
 from classes.base.AddValues import AddValues
-from pymysqlpool import ConnectionPool
 
 class DataUtils(AddValues):
     def __init__(self, values):
         self.values = values
         super().__init__(self.values)
-        self.pool = None
+        self.connection = None
         
         self.check_tables()
         
     ###########################################
     # Create project tables
     def check_tables(self):
-        pass # Better to do in command, as this could be used globally in py_frame
+        pass #Better to do in command, as this could be used globaly in py_frame
             
     ###########################################            
         
-    def get_mysql_connection(self):
-        if not self.pool:
-            login_info = self.values.get('config.config.login_info')
-            pool_params = {
-                'host': login_info['host'],
-                'port': login_info['port'],
-                'user': login_info['user'],
-                'password': login_info['password'],
-                'database': login_info['database'], 
-                'connect_timeout': 120,
-                'read_timeout': None,
-                'write_timeout': None,
+    def get_mysql_connection(self, max_retries=3):
+        for _ in range(max_retries):
+            if not self.connection or not self.connection.open:
+                login_info = self.values.get('config.config.login_info')
+                local_info = {
+                    'host': login_info['host'],
+                    'port': login_info['port'],
+                    'user': login_info['user'],
+                    'password': login_info['password'],
+                    'database': login_info['database'], 
+                    'connect_timeout': 120
                 }
-            self.pool = ConnectionPool(**pool_params)
+                
+                # Attempt to create a new connection
+                try:
+                    self.connection = pymysql.connect(**local_info)
+                except pymysql.Error as e:
+                    raise Exception(f"Failed to establish a new MySQL connection: {str(e)}")
 
-        return self.pool.get_connection()
+            try:
+                # Attempt to execute a simple query to check if the connection is alive
+                cursor = self.connection.cursor()
+                cursor.execute("SELECT 1")
+                cursor.close()
+                return self.connection  # Connection is alive, return it
+            except pymysql.Error as e:
+                # If an exception is raised, it indicates that the connection is dead
+                self.debug(f"Failed to ping MySQL server: {str(e)}")
+                self.connection.close()
+                time.sleep(10)  # Add a delay before retrying
+        raise Exception(f"Failed to ping MySQL server after {max_retries} retries. Connection may be dead.")
+
 
     def sql_dict(self, query, args=None, return_data=False, many=False, warn_seconds=2, return_id=False):
-        connection = None
-        cursor = None
         try:
             connection = self.get_mysql_connection()
             cursor = connection.cursor()
@@ -77,16 +90,14 @@ class DataUtils(AddValues):
                 return {row[0]: dict(zip(columns[1:], row[1:])) for row in result}
             return
         except Exception as e:
-            self.log(f"Error running sql: {e} {query}", level='error')
+            query2 = re.sub(r'\s+', ' ', query)[:100]
+            self.log(f"Error running sql: {e} {query2}", level='error')
             return None
         finally:
             if cursor:
                 cursor.close()
-            if connection:
-                connection.close()
-
-    def sql(self, query, args=None, return_data=False, many=False, warn_seconds=2, return_id=False):
-        connection = None
+                
+    def sql(self, query, args=None, return_data=False, many=False, warn_seconds=2,return_id=False):
         cursor = None
         try:
             connection = self.get_mysql_connection()
@@ -122,10 +133,9 @@ class DataUtils(AddValues):
                 return cursor.fetchall()
             return
         except Exception as e:            
-            self.log(f"Error running sql: {e} {query} {args}", level='error')
+            query2 = re.sub(r'\s+', ' ', query)[:100]
+            self.log(f"Error running sql: {e} {query2}", level='error')
             return None
         finally:
             if cursor:
                 cursor.close()
-            if connection:
-                connection.close()
